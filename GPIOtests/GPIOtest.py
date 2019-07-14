51bc0127c9b8a582487fa425795f48433f4941cb
Jake Bernstein <jgbernstein@gmail.com>
2019-07-14 15:40:35 -0400

    GPIOtest code ready to start debugging on pi

--- GPIOtests/GPIOtest.py
+++ GPIOtests/GPIOtest.py
@@ -2,19 +2,20 @@
 #Radiometer RPi GPIO test for photon counter and latch
 #July 14, 2019
 
-#Uses pigpio library, which needs daemon for python wrapper
-#Before running this function, call:
+#Uses pigpio library, which needs daemon for python wrapper#Before running this function, call:
 #'sudo pigpiod -s 1'
-
-import numpy as np
-#import pigpio
 import sys
+sys.path.append("../pigpio")
+import numpy as np
+import pigpio
+
 import time
 
 import radGPIO
 
-#pig = pigpio.pi()
-if len(sys.argv) == 0:
+pig = pigpio.pi()
+initCounterGPIO(pig)
+if len(sys.argv) == 1:
     nloops = 2
 else:
     nloops = int(sys.argv[1])
@@ -23,7 +24,8 @@ else:
 #print(str(nloops))
     
 for i in range(nloops):
-    radGPIO.countPhotons()
+    numphotons = radGPIO.countPhotons(pig)
+    print(str(numphotons))
     time.sleep(1)
 
 
--- GPIOtests/radGPIO.py
+++ GPIOtests/radGPIO.py
@@ -1,10 +1,37 @@
 #Jake Bernstein
+import sys
+sys.path.append("../pigpio")
 
-PIN_nOUTEN = 1
-PIN_LATCHEN = 2
-PIN_COUNTCLEAR = 3
-PINS_LATCHDAT = [4, 5, 6, 7, 8, 9, 10, 11]
+import numpy as np
 
-def countPhotons():
+PIN_nOUTEN = 21
+PIN_LATCHEN = 20
+PIN_COUNTCLEAR = 16
+PINS_LATCHDAT = [4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26]
+
+
+def initCounterGPIO(pi):
+    for pin in PINS_LATCHDAT:
+        pi.set_mode(pin, pigpio.INPUT)
+        pi.set_pull_up_down(pin, pigpio.PUD_OFF)
+        
+    for pin in [PIN_nOUTEN, PIN_LATCHEN]:
+        pi.set_mode(pin, pigpio.OUTPUT)
+
+    pi.write(PIN_nOUTEN,0)
+    pi.write(PIN_LATCHEN,0)
+
+def countPhotons(pi):
     print("Counting Photons")    
+    pi.write(PIN_LATCHEN,1)
+    pi.write(PIN_COUNTCLEAR,1)
+    pi.write(PIN_COUNTCLEAR,0)
+    rawdat = pi.read_bank_1()
+    return orderbits(np.uint32(rawdat))
 
+def orderbits(datin):
+    datout = np.int32(0)
+    for i in range(len(PINS_LATCHDAT)):
+        bitdat = np.bitwise_and(1, np.right_shift(datin, PINS_LATCHDAT[i]))
+        datout = datout + np.left_shift(bitdat,i)
+        
